6b2bf2597b2fb4284977a4650ec5623a0c9f5958
Andres Hidalgo <andrew.ahc22@gmail.com>
2020-09-25 21:34:28 -0500

    calculadora

---
+++ suma.py
@@ -0,0 +1,25 @@
+#Scrip : calculado
+
+'''
+operaciones basicas con dos numeros
+designados
+'''
+
+n1=5
+n2=6
+
+#process
+suma=n1+n2
+resta=n1-n2
+multiplicacion=n1*n2
+division=n1/n2
+
+#output
+print ('el resultado de la susma es ',suma)
+print ('el resultado de la resta es ',resta)
+print ('el resultado de la multiplicaicon es ',multiplicacion)
+print ('el resultado de la division es ',division)
+
+
+
+
