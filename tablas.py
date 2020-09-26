1fb0165cd96200a4f1ceb83aa56f406103fae5a1
Andres Hidalgo <andrew.ahc22@gmail.com>
2020-09-25 23:49:32 -0500

    tablas

---
+++ tablas.py
@@ -0,0 +1,19 @@
+#script : tablas de multiplicar
+
+'''
+programa que calcula las tablas de multiplicar
+'''
+print ('digite un numero entre el 1 y 5 para indicar su respectiva tabla de multiplicar')
+num= int (input())
+
+print (num, " x 1 =",num*1)
+print (num, " x 2 =",num*2)
+print (num, " x 3 =",num*3)
+print (num, " x 4 =",num*4)
+print (num, " x 5 =",num*5)
+print (num, " x 6 =",num*6)
+print (num, " x 7 =",num*7)
+print (num, " x 8 =",num*8)
+print (num, " x 9 =",num*9)
+print (num, " x 10 =",num*10)
+
